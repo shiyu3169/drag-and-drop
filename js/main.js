45503c4b1a4db9378f0300bd33f5831cd60ac284
Shiyu Wang <shiyu3169@gmail.com>
2019-07-29 14:23:08 -0400

    js for drag

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+// Fill Listeners
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+// Drag Functions
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
