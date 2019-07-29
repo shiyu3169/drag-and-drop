4117aed1f1c7e7e956dada2d3795be20a9fbf000
Shiyu Wang <shiyu3169@gmail.com>
2019-07-29 14:35:14 -0400

    finish

--- js/main.js
+++ js/main.js
@@ -5,12 +5,38 @@ const empties = document.querySelectorAll(".empty");
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
+// Loop through empties and call drag events
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
 // Drag Functions
 function dragStart() {
-  this.className += " hold";
+  this.classList.add("hold");
   setTimeout(() => (this.className = "invisible"), 0);
 }
 
 function dragEnd() {
   this.className = "fill";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.classList.add("hovered");
+}
+
+function dragLeave() {
+  this.className = "empty";
+}
+
+function dragDrop() {
+  this.className = "empty";
+  this.appendChild(fill);
 }
